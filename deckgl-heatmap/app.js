0ba68719ba6a664d2acd84a93e80f82e712be4da
edgarramirez <edgarramirez1509@gmail.com>
2023-07-31 12:05:13 -0700

    marker

--- deckgl-heatmap/app.js
+++ deckgl-heatmap/app.js
@@ -13,7 +13,6 @@ const map = new mapboxgl.Map({
   pitch: 0
 });
 
-
 const data = [
   {COORDINATES: [-122.42177834, 37.78346622], WEIGHT: 10},
   {COORDINATES: [-122.41346246584467, 37.79605716494423], WEIGHT: 10},
@@ -24,13 +23,23 @@ const heatmapLayer = new HeatmapLayer({
   data,
   getPosition: d => d.COORDINATES,
   getWeight: d => d.WEIGHT,
-  aggregation: 'SUM'
+  aggregation: 'SUM',
+  radiusPixels: 0.03 * 2**(map.getZoom()) 
 });
 
+map.on('zoom', () => {
+  let currentZoom = map.getZoom();
+  console.log("ZOOM CHANGED:", currentZoom);
+  // const newLayer = heatmapLayer.clone({ radiusPixels: 0.03 * 2**(currentZoom) });
+  const newLayer = heatmapLayer.clone({radiusPixels: 200 });
+  const refreshedLayers = []
+  refreshedLayers.push(newLayer);
+  heatmapLayer.context.deck.setProps({ layers: refreshedLayers })
+});
 
 const deckOverlay = new DeckOverlay({
   layers: [
-    heatmapLayer
+    heatmapLayer    
   ]
 });
 
---
+++ issue#8/app.js
@@ -0,0 +1,11 @@
+import "./app.css";
+import Sidebar from "./Components/Sidebar";
+function app(){
+    return (
+        <h1>
+            hello
+        </h1>
+   
+    );
+}
+export default app;
